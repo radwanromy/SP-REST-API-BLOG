ae57de93e6589391cb50a2a04305ca23fa6ca73e
radwanromy <radwanromy@gmail.com>
2021-12-20 13:12:05 +0600

    SP API Blog 008

--- src/main/java/com/springboot/blog/service/impl/PostService.java
+++ src/main/java/com/springboot/blog/service/impl/PostService.java
@@ -9,4 +9,5 @@ public interface PostService {
     List<PostDto> getAllPosts();
 
     PostDto getPostById(long id);
+    PostDto updatePost(PostDto postDto, long id);
 }
--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -48,6 +48,18 @@ public class PostServiceImpl implements PostService{
         return mapToDTO(post);
     }
 
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        // get Post by Id from the database
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post","id", id));
+
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        Post updatedPost = postRepository.save(post);
+        return mapToDTO(updatedPost);
+    }
+
     //convert Entity Into DTO
     private PostDto mapToDTO(Post post){
         PostDto postDto = new PostDto();
