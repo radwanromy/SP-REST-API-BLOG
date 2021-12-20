cd77f1d2316db9171aabcc8c4c02301b98a33bff
radwanromy <radwanromy@gmail.com>
2021-12-20 11:37:12 +0600

    SP API Blog 002

--- src/main/java/com/springboot/blog/service/impl/PostService.java
+++ src/main/java/com/springboot/blog/service/impl/PostService.java
@@ -2,6 +2,9 @@ package com.springboot.blog.service.impl;
 
 import com.springboot.blog.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+    List<PostDto> getAllPosts();
 }
--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -5,6 +5,9 @@ import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class PostServiceImpl implements PostService{
     private PostRepository postRepository;
@@ -32,5 +35,18 @@ public class PostServiceImpl implements PostService{
 
 
         return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        return null;
+    }
+    private PostDto mapToDTO(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
     }
 }
